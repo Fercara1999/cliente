9caee228ed9421ecd273d486ea347c0b4d417e06
Fercara1999 <fercara1999@gmail.com>
2023-10-06 18:22:52 +0200

    Cambios 6O

--- Tarea006FernandoCalles/js/funciones.js
+++ Tarea006FernandoCalles/js/funciones.js
@@ -1,12 +1,89 @@
-function romboConfigurable(){
+// Desarrolla una página que muestre el número de filas que contendrá un rombo hueco (imagen superior izquierda).
+// Cuando el usuario cambie el valor del input, que cambie el rombo. Los rombos “se dibujan” con el carácter asterisco.
 
-    let nLineas = 10;
+// Función para elegir que función tomar decidiendo si el número es o no par
+function eligeFuncion(){
+  let nLineas = parseInt(document.getElementById('n1').value);
 
-    for (let i = 0 ; i < nLineas ; i++){
-        document.write("*");
-
-    }
-
+  if (nLineas <= 2 ){
+    window.alert("Has introducido un número demasiado bajo, no se puede hacer un rombo con menos de 3 líneas");
+  }else if (nLineas % 2 == 0){
+    generaRomboLineasPar();
+  }else{
+    generaRomboLineasImpar();
+  }
 }
 
-romboConfigurable();
+// Función si es par
+function generaRomboLineasPar(){
+
+  let nLineas = parseInt(document.getElementById('n1').value);
+  let lineasPar = nLineas /2;
+  let resultado = "";
+
+  //Generación de la parte de arriba
+  for(let i = 1 ; i <= lineasPar ; i++){
+    for(let espacios = lineasPar-1 ; espacios >= i;espacios--){
+      resultado+="&nbsp";
+    }
+    for(let asteriscos = 1; asteriscos <= i*2-1 ; asteriscos++){
+      if (asteriscos != 1){
+        resultado+="&nbsp";
+      }else{
+      resultado+="*";
+      }
+    }
+    resultado+="<br>";
+  }
+  //Generación de la parte de abajo
+  for(let i = lineasPar ; i >= 1 ; i--){
+    for(let espacios = 0 ; espacios < lineasPar-i ; espacios++){
+      resultado+='&nbsp';
+    }
+    for(let asteriscos = i*2-1 ; asteriscos >= 1 ; asteriscos--){
+      resultado+="*";
+    }
+    resultado+="<br>";
+  }
+
+  document.getElementById('rombo').innerHTML = resultado;
+}
+
+// Función si es impar
+function generaRomboLineasImpar(){
+
+  let nLineas = parseInt(document.getElementById('n1').value);
+  let lineasImpar = Math.trunc(nLineas/2);
+  console.log(lineasImpar);
+  let resultado = "";
+
+  //Generación de la parte de arriba
+  for(let i = 1 ; i <= lineasImpar ; i++){
+    for(let espacios = lineasImpar ; espacios >= i;espacios--){
+      resultado+="&nbsp";
+    }
+    for(let asteriscos = 1; asteriscos <= i*2-1 ; asteriscos++){
+      resultado+="*";
+    }
+    resultado+="<br>";
+  }
+  //Generación de la línea intermedia
+  for(let m = 1 ; m <= nLineas ; m++){
+    resultado+= "*";
+  }
+  resultado+="<br>";
+  //Generación de la parte de abajo
+  for(let i = lineasImpar ; i >= 1 ; i--){
+    for(let espacios = 0 ; espacios <= lineasImpar-i ; espacios++){
+      resultado+='&nbsp';
+    }
+    for(let asteriscos = i*2-1 ; asteriscos >= 1 ; asteriscos--){
+      resultado+="*";
+    }
+    resultado+="<br>";
+  }
+
+  document.getElementById('rombo').innerHTML = resultado;
+}
+
+generaRomboLineasImpar();
